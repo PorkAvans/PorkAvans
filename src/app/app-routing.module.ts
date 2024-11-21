166af6ef398fea770e9082986d26bb6f65801cb8
daclechaca99 <danielcchaconc@gmail.com>
2024-11-21 00:57:22 -0500

    se realizan ajustes de front y se crean lso guardianes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,26 +7,24 @@ import { CollectionDashboardComponent } from './collection/collection-dashboard/
 import { ComidaSuministradaComponent } from './comida-suministrada/comida-suministrada.component';
 import { StockComponent } from './stock-products/stock-products.component';
 import { AddAfiliadoComponent } from './gestion_afiliado/add-afiliado/add-afiliado.component';
+import { AuthGuard } from './auth.guard';
 
-// Aquí está la modificación para la ruta de gestion-distribuidores
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'recoleccion', component: RecoleccionComponent },
-  { path: 'collection-dashboard', component: CollectionDashboardComponent },
-  { path: 'comida-suministrada', component: ComidaSuministradaComponent },
-  { path: 'stock-products', component: StockComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }, // Protegida
+  { path: 'recoleccion', component: RecoleccionComponent, canActivate: [AuthGuard] }, // Protegida
+  { path: 'collection-dashboard', component: CollectionDashboardComponent, canActivate: [AuthGuard] }, // Protegida
+  { path: 'comida-suministrada', component: ComidaSuministradaComponent, canActivate: [AuthGuard] }, // Protegida
+  { path: 'stock-products', component: StockComponent, canActivate: [AuthGuard] }, // Protegida
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'add-afiliado', component: AddAfiliadoComponent },
+  { path: 'add-afiliado', component: AddAfiliadoComponent, canActivate: [AuthGuard] }, // Protegida
   
-  
-  // Modificación aquí
-  { path: 'gestion-distribuidores', loadChildren: () => import('./gestion-distribuidores/gestion-distribuidores.module').then(m => m.GestionDistribuidoresModule) },
-  { path: 'gestion_usuarios', loadChildren: () => import('./gestion_usuarios/gestion-usuarios.module').then(m => m.GestionUsuariosModule) },
-  { path: 'gestion_afiliado', loadChildren: () => import('./gestion_afiliado/gestion-afiliado.module').then(m => m.GestionAfiliadoModule) },
-  { path: 'gestion-ventas', loadChildren: () => import('./gestion-ventas/gestion-ventas.module').then(m => m.GestionVentasModule) },
-  { path: 'gestion-coleccion', loadChildren: () => import('./gestion-coleccion/gestion-coleccion.module').then(m => m.GestionColeccionModule) 
-  },
+  // Rutas protegidas con lazy loading
+  { path: 'gestion-distribuidores', loadChildren: () => import('./gestion-distribuidores/gestion-distribuidores.module').then(m => m.GestionDistribuidoresModule), canActivate: [AuthGuard] },
+  { path: 'gestion_usuarios', loadChildren: () => import('./gestion_usuarios/gestion-usuarios.module').then(m => m.GestionUsuariosModule), canActivate: [AuthGuard] },
+  { path: 'gestion_afiliado', loadChildren: () => import('./gestion_afiliado/gestion-afiliado.module').then(m => m.GestionAfiliadoModule), canActivate: [AuthGuard] },
+  { path: 'gestion-ventas', loadChildren: () => import('./gestion-ventas/gestion-ventas.module').then(m => m.GestionVentasModule), canActivate: [AuthGuard] },
+  { path: 'gestion-coleccion', loadChildren: () => import('./gestion-coleccion/gestion-coleccion.module').then(m => m.GestionColeccionModule), canActivate: [AuthGuard] },
   
   { path: '**', redirectTo: '/login' }
 ];
